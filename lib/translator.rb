787f5e24623bea4f2e0ea0f918222a303bc1e578
Lauren Yu <laurenkyu@gmail.com>
2020-01-24 04:45:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(file_path, eng_emoticon)
   if library["get_emoticon"].include?(eng_emoticon)
     library["get_emoticon"][eng_emoticon]
   else
-    print "Sorry, that emoticon was not found"
+    p "Sorry, that emoticon was not found"
   end 
 end 
 
