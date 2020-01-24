b4365650eeb8233db601eb1a3b07b05c9498c77d
Lauren Yu <laurenkyu@gmail.com>
2020-01-24 04:36:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   library = load_library(file_path)
-  if library[get_meaning].include? 
+  if library[get_emoticon].include? {
 
 def get_english_meaning
   # code goes here
