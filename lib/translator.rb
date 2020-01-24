7604e51f2e09e375daba08de58c6421c56820b72
Lauren Yu <laurenkyu@gmail.com>
2020-01-24 04:21:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,10 +3,7 @@ require "yaml"
 def load_library(yaml_file)
   library = YAML.load_file(yaml_file)
   new_hash = {"get_meaning" => {}, "get_emoticon" => {} }
-  library.each do
-    |english, emoticons| 
-    library["get_meaning"][emoticons] = english
-  end 
+ p library 
 end
 
 def get_japanese_emoticon
