86bc0b58ad45313c77a306025b78e42263d358bf
Lauren Yu <laurenkyu@gmail.com>
2020-01-24 04:15:57 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ def load_library(yaml_file)
   new_hash = {"get_meaning" => {}, "get_emoticon" => {} }
   library.each do
     |key, value| 
-    library["get_meaning"][]
+    library["get_meaning"][value]
   
 end
 
